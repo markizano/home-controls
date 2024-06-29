a09b3f28d540eee996201dc1d4e9160e609c575c
Markizano Draconus <markizano@markizano.net>
2024-06-29 12:12:55 -0500

    ui components to display and render the controls panel.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type VolumeResponse = { status: number, message: string, volume: number[] };
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, HttpClientModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'home-controls';
+    title = 'home-controls';
+    vol = 0;
+
+    constructor(protected httpClient: HttpClient) { }
+
+    ngOnInit() {
+        this.httpClient.get<VolumeResponse>('/api/volume', {}).subscribe((x: VolumeResponse) => {
+            console.log(x);
+            this.vol = x.volume[0] / 65534;
+        });
+    }
+
+    volumeUp() {
+        this.httpClient.get<VolumeResponse>('/api/volume/up', {}).subscribe((x: VolumeResponse) => {
+            console.log(x);
+            this.vol = x.volume[0] / 65534;
+        });
+    }
+
+    volumeDown() {
+        this.httpClient.get<VolumeResponse>('/api/volume/down', {}).subscribe((x) => {
+            console.log(x);
+            this.vol = x.volume[0] / 65534;
+        });
+    }
 }
